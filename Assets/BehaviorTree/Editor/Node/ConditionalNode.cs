e79cd51e170598551ba8aae1e6bda03b24231e72
SasakiHayato0605 <zuomusundou685@gmail.com>
2022-07-06 20:02:17 +0900

    Portの作成

--- Assets/BehaviorTree/Editor/Node/ConditionalNode.cs
+++ Assets/BehaviorTree/Editor/Node/ConditionalNode.cs
@@ -1,9 +1,12 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEditor.Experimental.GraphView;
 
 public class ConditionalNode : NodeBase
 {
+    
     public ConditionalNode() : base() { }
+
     protected override string SetPath() => "Condition";
 }
--- Assets/BehaviorTree/Editor/Node/NodeBase.cs
+++ Assets/BehaviorTree/Editor/Node/NodeBase.cs
@@ -3,14 +3,44 @@ using UnityEngine.UIElements;
 
 public abstract class NodeBase : Node
 {
+    class PortController
+    {
+        const string InputName = "ì¸óÕ";
+        const string OutPutName = "èoóÕ";
+
+        public Port Input { get; private set; }
+        public Port Output { get; private set; }
+
+        System.Type a;
+
+        public PortController()
+        {
+            Input = CreatePort(InputName, Direction.Input, Orientation.Horizontal, Port.Capacity.Single);
+            Output = CreatePort(OutPutName, Direction.Output, Orientation.Horizontal, Port.Capacity.Multi);
+        }
+
+        private Port CreatePort(string name, Direction direction, Orientation orientation = Orientation.Horizontal, Port.Capacity capacity = Port.Capacity.Single)
+        {
+            var port = Port.Create<PortData>(orientation, direction, capacity, a);
+            port.portName = name;
+
+            return port;
+        }
+    }
+
+    PortController _portController;
+
     public NodeBase()
     {
         title = Path;
-
         mainContainer.Add(new TextField(title));
+
+        _portController = new PortController();
+
+        inputContainer.Add(_portController.Input);
+        outputContainer.Add(_portController.Output);
     }
 
-    string _path;
     internal string Path 
     { 
         get
